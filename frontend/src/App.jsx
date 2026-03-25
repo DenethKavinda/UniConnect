ec9ef33ba9d2ccd3612bd3cfb2fc6730f04d6486
Deneth Kavinda <it23571402@my.sliit.lk>
2026-03-25 12:09:34 +0530

    Temporary save before switching branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Dashboard from "./pages/Dashboard";
 import Group from "./pages/Group";
-import CreateGroup from "./pages/CreateGroup";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <main className="pt-[70px] min-h-screen">
           <Routes>
             <Route path="/" element={<Dashboard />} />
-            <Route path="/groups" element={<Group />} />
             <Route path="*" element={<Dashboard />} />
           </Routes>
         </main>
